9159ccf72615a1c2f92c323963b56f464e676d48
Alexis Guyomar <alexis.guyomar@prestashop.com>
2025-01-31 11:43:46 +0100

    feat: add restore configuration validator

---
+++ classes/Parameters/RestoreConfigurationValidator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace PrestaShop\Module\AutoUpgrade\Parameters;
+
+use PrestaShop\Module\AutoUpgrade\Backup\BackupFinder;
+use PrestaShop\Module\AutoUpgrade\UpgradeTools\Translator;
+
+class RestoreConfigurationValidator
+{
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    /**
+     * @var BackupFinder
+     */
+    private $backupFinder;
+
+    public function __construct(Translator $translator, BackupFinder $backupFinder)
+    {
+        $this->translator = $translator;
+        $this->backupFinder = $backupFinder;
+    }
+
+    /**
+     * @param array<string, mixed> $array
+     *
+     * @return array<array{'message': string, 'target': string}>
+     */
+    public function validate(array $array = []): array
+    {
+        $errors = [];
+
+        $backupNameErrors = $this->validateBackupName($array);
+        if ($backupNameErrors) {
+            $errors[] = $backupNameErrors;
+
+            return $errors;
+        }
+
+        $backupNameExistErrors = $this->validateBackupExist($array[RestoreConfiguration::BACKUP_NAME]);
+        if ($backupNameExistErrors) {
+            $errors[] = $backupNameExistErrors;
+        }
+
+        return $errors;
+    }
+
+    /**
+     * @param array<string, mixed> $backupName
+     * @return string|null
+     */
+    private function validateBackupName(array $backupConfiguration): ?string
+    {
+        if (empty($backupConfiguration[RestoreConfiguration::BACKUP_NAME])) {
+            return $this->translator->trans('Backup name is missing');
+        }
+
+        return null;
+    }
+
+    private function validateBackupExist(string $backupName): ?string
+    {
+        if (!in_array($backupName, $this->backupFinder->getAvailableBackups())) {
+            return $this->translator->trans('Backup %s does not exist', [$backupName]);
+        }
+
+        return null;
+    }
+}
---
+++ tests/unit/Parameters/RestoreConfigurationValidatorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PrestaShop\Module\AutoUpgrade\Parameters\RestoreConfiguration;
+use PrestaShop\Module\AutoUpgrade\Backup\BackupFinder;
+use PrestaShop\Module\AutoUpgrade\Parameters\RestoreConfigurationValidator;
+use PrestaShop\Module\AutoUpgrade\UpgradeContainer;
+
+class RestoreConfigurationValidatorTest extends TestCase
+{
+    /**
+     * @var RestoreConfigurationValidator
+     */
+    private $validator;
+
+    private $backupFinderMock;
+
+    protected function setUp(): void
+    {
+        $this->container = new UpgradeContainer('/html', '/html/admin');
+
+        $this->backupFinderMock = $this->createMock(BackupFinder::class);
+
+        $this->validator = new RestoreConfigurationValidator(
+            $this->container->getTranslator(),
+            $this->backupFinderMock
+        );
+    }
+
+    public function testValidateReturnsErrorWhenBackupNameIsMissing(): void
+    {
+        $errors = $this->validator->validate([]);
+
+        $this->assertCount(1, $errors);
+        $this->assertSame('Backup name is missing', $errors[0]);
+    }
+
+    public function testValidateReturnsErrorWhenBackupDoesNotExist(): void
+    {
+        $backupName = 'non_existing_backup.zip';
+
+        $this->backupFinderMock
+            ->method('getAvailableBackups')
+            ->willReturn(['existing_backup.zip']);
+
+        $errors = $this->validator->validate([RestoreConfiguration::BACKUP_NAME => $backupName]);
+
+        $this->assertCount(1, $errors);
+        $this->assertSame('Backup non_existing_backup.zip does not exist', $errors[0]);
+    }
+
+    public function testValidateReturnsNoErrorsWhenBackupIsValid(): void
+    {
+        $backupName = 'existing_backup.zip';
+
+        $this->backupFinderMock
+            ->method('getAvailableBackups')
+            ->willReturn([$backupName]);
+
+        $errors = $this->validator->validate([RestoreConfiguration::BACKUP_NAME => $backupName]);
+
+        $this->assertCount(0, $errors);
+    }
+}
