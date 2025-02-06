dce9a4571340d147e4ef86e3cf27ef5fa45171c7
Alexis Guyomar <alexis.guyomar@prestashop.com>
2025-02-06 12:42:09 +0100

    fix: stepper to check init stepper again if it change.

--- _dev/src/ts/utils/Stepper.ts
+++ _dev/src/ts/utils/Stepper.ts
@@ -19,36 +19,38 @@
 import type { Step } from '../types/Stepper';
 
 export default class Stepper {
-  private stepper: HTMLDivElement;
-  private steps: Step[];
+  #stepper: HTMLDivElement | null = null;
+  #steps: Step[] = [];
 
-  private baseClass = 'stepper__step';
-  private currentClass = `${this.baseClass}--current`;
-  private doneClass = `${this.baseClass}--done`;
-  private normalClass = `${this.baseClass}--normal`;
+  #baseClass = 'stepper__step';
+  #currentClass = `${this.#baseClass}--current`;
+  #doneClass = `${this.#baseClass}--done`;
+  #normalClass = `${this.#baseClass}--normal`;
+
+  constructor() {
+    this.#initStepper();
+  }
 
   /**
-   * @constructor
-   * @throws Will throw an error if the stepper or its steps are not found in the DOM.
+   * @private
+   * @throws Error Will throw an error if the stepper or its steps are not found in the DOM.
    * @description Initializes the Stepper by finding the parent element in the DOM and setting up the steps.
    */
-  constructor() {
-    const stepper = document.getElementById(
+  #initStepper = (): void => {
+    this.#stepper = document.getElementById(
       window.AutoUpgradeVariables.stepper_parent_id
     ) as HTMLDivElement | null;
-    if (!stepper) {
+    if (!this.#stepper) {
       throw new Error("The stepper wasn't found inside DOM. stepper can't be initiated properly");
     }
 
-    this.stepper = stepper;
-
-    const domSteps = Array.from(this.stepper.children) as HTMLElement[];
+    const domSteps = Array.from(this.#stepper.children) as HTMLElement[];
 
     if (!domSteps.length) {
       throw new Error("The stepper hasn't steps inside DOM. stepper can't be initiated properly");
     }
 
-    this.steps = domSteps.map((step) => {
+    this.#steps = domSteps.map((step) => {
       const stepCode = step.dataset.stepCode;
       if (!stepCode) {
         throw new Error(
@@ -60,7 +62,7 @@ export default class Stepper {
         element: step
       };
     });
-  }
+  };
 
   /**
    * @public
@@ -68,35 +70,44 @@ export default class Stepper {
    * @description Sets the current step in the stepper and updates the classes for each step accordingly.
    */
   public setCurrentStep = (currentStep: string) => {
-    const stepIndex = this.steps.findIndex((step) => step.code === currentStep);
+    if (this.#getStepIndex(currentStep) === -1) {
+      this.#initStepper();
+    }
+
+    const stepIndex = this.#getStepIndex(currentStep);
 
     if (stepIndex === -1) {
       console.debug(`Step ${currentStep} not found in list.`);
       return;
     }
-    this.stepper.classList.add('stepper--hydration');
 
-    this.steps.forEach((step, i) => {
+    this.#stepper?.classList.add('stepper--hydration');
+
+    this.#steps.forEach((step, i) => {
       const { element } = step;
 
       const newClass = this.#getClassOfStep(stepIndex, i);
 
       if (!element.classList.contains(newClass)) {
-        element.classList.remove(this.currentClass, this.doneClass, this.normalClass);
+        element.classList.remove(this.#currentClass, this.#doneClass, this.#normalClass);
         element.classList.add(newClass);
       }
     });
   };
 
+  #getStepIndex = (stepToGetIndex: string): number => {
+    return this.#steps.findIndex((step) => step.code === stepToGetIndex);
+  };
+
   #getClassOfStep(referenceIndex: number, currentIndex: number): string {
     if (currentIndex === referenceIndex) {
-      return this.currentClass;
+      return this.#currentClass;
     }
 
     if (currentIndex < referenceIndex) {
-      return this.doneClass;
+      return this.#doneClass;
     }
 
-    return this.normalClass;
+    return this.#normalClass;
   }
 }
